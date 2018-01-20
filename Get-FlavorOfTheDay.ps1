a601d8d596d452e1bc9feebb0032cfae06bac37e
Max Anderson <cctpmax@gmail.com>
2018-01-19 19:50:59 -0500

    Script now outputs an object with two properties: Resturant and Flavor

--- Get-FlavorOfTheDay.ps1
+++ Get-FlavorOfTheDay.ps1
@@ -7,19 +7,45 @@ PARAM (
   [Switch]$FindResturant
 )
 
-If ($FindResturant) {
-  Start-Process "http://www.culvers.com/locator/view-all-locations"
-  Exit
+Begin {
+
+  #Create an Empty Array
+  $Output = @()
+
 }
 
-
-#Runs the Invoke-WebRequest on the requested resturant
-$WebRequest = Invoke-WebRequest -Uri "http://www.culvers.com/restaurants/$Resturant"
+Process {
   
-#Checks to see if the page is a 404
-If ($WebRequest.ParsedHTML.Title -eq "Flavor of the Day | Culver's") {
-  Throw "Resturant `"$Resturant`" not found!"
+  If ($FindResturant) {
+    Start-Process "http://www.culvers.com/locator/view-all-locations"
+    Exit
+  }
+
+
+  #Runs the Invoke-WebRequest on the requested resturant
+  $WebRequest = Invoke-WebRequest -Uri "http://www.culvers.com/restaurants/$Resturant"
+    
+  #Checks to see if the page is a 404
+  If ($WebRequest.ParsedHTML.Title -eq "Flavor of the Day | Culver's") {
+    Throw "Resturant `"$Resturant`" not found!"
+  }
+
+  #Outputs the Flavor of the day parsed from the web request
+  $FlavorOTD = (($WebRequest.ParsedHtml.getElementsByTagName("DIV") | Where-Object {$_.className -eq 'ModuleRestaurantDetail-fotd'}).InnerText).SubString(30)
+
+  $FlavorOTD_Object = [pscustomobject] @{
+
+    "Resturant" = $Resturant
+    "Flavor" = $FlavorOTD
+
+  }
+
+  $Output += $FlavorOTD_Object
+
+  Write-Output $Output
+
 }
 
-#Outputs the Flavor of the day parsed from the web request
-(($WebRequest.ParsedHtml.getElementsByTagName("DIV") | Where-Object {$_.className -eq 'ModuleRestaurantDetail-fotd'}).InnerText).SubString(30)
+End {
+
+}
