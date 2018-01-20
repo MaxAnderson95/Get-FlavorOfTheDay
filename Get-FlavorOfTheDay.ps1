909f7e1671e56fb51a68d04e16357494b67315dc
Max Anderson <cctpmax@gmail.com>
2018-01-19 20:08:13 -0500

    Now allows an array of resturants to be queried using the -Resturant param

--- Get-FlavorOfTheDay.ps1
+++ Get-FlavorOfTheDay.ps1
@@ -1,7 +1,7 @@
 [Cmdletbinding()]
 PARAM (
   [Parameter(Mandatory=$True,Position=0,ParameterSetName='EnterResturant')]
-  [String]$Resturant,
+  [String[]]$Resturant,
 
   [Parameter(Mandatory=$False,Position=0,ParameterSetName='FindResturant')]
   [Switch]$FindResturant
@@ -16,33 +16,40 @@ Begin {
 
 Process {
   
-  If ($FindResturant) {
-    Start-Process "http://www.culvers.com/locator/view-all-locations"
-    Exit
+  ForEach ($Resturant_Current in $Resturant) {
+
+    If ($FindResturant) {
+      Start-Process "http://www.culvers.com/locator/view-all-locations"
+      Exit
+    }
+
+
+    #Runs the Invoke-WebRequest on the requested resturant
+    $WebRequest = Invoke-WebRequest -Uri "http://www.culvers.com/restaurants/$Resturant_Current"
+      
+    #Checks to see if the page is a 404
+    If ($WebRequest.ParsedHTML.Title -eq "Flavor of the Day | Culver's") {
+      Break "Resturant `"$Resturant`" not found!"
+    }
+
+    #Outputs the Flavor of the day parsed from the web request
+    $FlavorOTD = (($WebRequest.ParsedHtml.getElementsByTagName("DIV") | Where-Object {$_.className -eq 'ModuleRestaurantDetail-fotd'}).InnerText).SubString(30)
+
+    #Create a custom object with two properties
+    $FlavorOTD_Object = [pscustomobject] @{
+
+      "Resturant" = $Resturant_Current
+      "Flavor" = $FlavorOTD
+
+    }
+
+    #Add the custom object to the array created earlier
+    $Output += $FlavorOTD_Object
+
   }
 
-
-  #Runs the Invoke-WebRequest on the requested resturant
-  $WebRequest = Invoke-WebRequest -Uri "http://www.culvers.com/restaurants/$Resturant"
-    
-  #Checks to see if the page is a 404
-  If ($WebRequest.ParsedHTML.Title -eq "Flavor of the Day | Culver's") {
-    Throw "Resturant `"$Resturant`" not found!"
-  }
-
-  #Outputs the Flavor of the day parsed from the web request
-  $FlavorOTD = (($WebRequest.ParsedHtml.getElementsByTagName("DIV") | Where-Object {$_.className -eq 'ModuleRestaurantDetail-fotd'}).InnerText).SubString(30)
-
-  $FlavorOTD_Object = [pscustomobject] @{
-
-    "Resturant" = $Resturant
-    "Flavor" = $FlavorOTD
-
-  }
-
-  $Output += $FlavorOTD_Object
-
-  Write-Output $Output
+  #Output the array  
+  Write-Output $Output  
 
 }
 
